94be22991073263b492142bce1caeba26e23331e
mvmxzy <mimozathuynh@gmail.com>
2023-08-20 14:47:22 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,26 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret"
+app.config['SECRET_KEY'] = '123'
 
-debug = DebugToolbarExtension(app)
+toolbar = DebugToolbarExtension(app)
 
-
-@app.route("/")
-def ask_questions():
-    """Generate and show form to ask words."""
-
+@app.route('/')
+def home():
+    """ Create and show form to ask for words for story """
     prompts = story.prompts
+    return render_template('home.html', prompts=prompts)
 
-    return render_template("questions.html", prompts=prompts)
-
-
-@app.route("/story")
-def show_story():
-    """Show story result."""
-
+@app.route('/story')
+def get_story():
+    """ Show story with words from form """
     text = story.generate(request.args)
-
-    return render_template("story.html", text=text)
+    return render_template('story.html', text=text)
